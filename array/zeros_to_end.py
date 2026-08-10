bbf7eda2367d5f27798ef205e7b9145fdab184e7
suryaprakash <b.7993974026@gmail.com>
2026-08-10 11:06:24 +0000

    initial commit

---
+++ array/zeros_to_end.py
@@ -0,0 +1,13 @@
+# we are moving zeros to the end 
+
+class Solution:
+    def pushZerosToEnd(self, arr):
+        # code here
+        count =0
+        for i in range(len(arr)):
+            if arr[i] !=0 :
+                arr[i],arr[count]=arr[count],arr[i]
+                
+                count +=1
+                
+               
