b6c5931d4612359a607f4c12827bb333ca5547f2
suryaprakash <b.7993974026@gmail.com>
2026-08-13 01:46:55 +0530

    Create rotate_Array.py

---
+++ array/rotate_Array.py
@@ -0,0 +1,25 @@
+AmazonMicrosoftMAQ SoftwareCodenation
+Topic Tags
+Related Interview Experiences
+Related Articles
+If you are facing any issue on this page. Please let us know.
+Output Window
+Runtime Error 
+Ask Yogi Bot
+Test Cases Passed: 
+0 /1115
+For Input : 
+1 2 3 4 5
+2
+Your Output:
+Traceback (most recent call last):
+  File "/home/guest/sandbox/6cb2e42b-efd0-476c-8515-e6aed13c5001/Solution.py", line 49, in <module>
+    main()
+    ~~~~^^
+  File "/home/guest/sandbox/6cb2e42b-efd0-476c-8515-e6aed13c5001/Solution.py", line 36, in main
+    ob.rotateArr(A, D)
+    ^^^^^^^^^^^^
+AttributeError: 'Solution' object has no attribute 'rotateArr'
+
+Its Correct output is:
+3 4 5 1 2
